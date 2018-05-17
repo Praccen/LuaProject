a1ea14cbf3d1856793cb61804ec873721e697c24
Emil Wahl <emilwahl@gmail.com>
2018-05-17 14:19:04 +0200

    Test

--- Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Scene.cpp
+++ Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Scene.cpp
@@ -13,7 +13,7 @@ Scene::~Scene() {
 
 void Scene::update() {
 	if (m_device->isWindowActive()) {
-		smgr->getActiveCamera()->setInputReceiverEnabled(true);
+		smgr->getCamera()->setInputReceiverEnabled(true);
 		m_device->getCursorControl()->setVisible(false);
 	}
 	else {

1e2598b970d843086875d03757415fe9373586b4
GUSTAV BJÖRK <gubj15@student.bth.se>
2018-05-18 15:08:40 +0200

    Added a getNodes function in scene. Added comments to the functions in scene

--- Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Scene.h
+++ Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Scene.h
@@ -3,6 +3,12 @@
 #include <iostream>
 #include <irrlicht.h>
 #include <vector>
+#include <string>
+
+struct nodeInfo {
+	std::string name;
+	int id;
+};
 
 class Scene {
 private:
@@ -18,6 +24,8 @@ public:
 	void addMesh(std::vector<std::vector<irr::core::vector3df>> triangles);
 	void addBox(irr::core::vector3df position, float size, std::string name);
 	void setCamera(irr::core::vector3df position, irr::core::vector3df target);
+
+	std::vector<nodeInfo> getNodes();
 
 	void update();
 	void draw();

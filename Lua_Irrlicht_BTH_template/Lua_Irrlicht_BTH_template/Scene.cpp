807fa93d8a453ab7a4d81893a183057a926c9842
GUSTAV BJÖRK <gubj15@student.bth.se>
2018-05-18 12:53:39 +0200

    Made each node have the same buffer instead of every triangle having their own buffer.

--- Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Scene.cpp
+++ Lua_Irrlicht_BTH_template/Lua_Irrlicht_BTH_template/Scene.cpp
@@ -21,47 +21,44 @@ Scene::~Scene() {
 }
 
 void Scene::addMesh(std::vector<std::vector<core::vector3df>> triangles) {
+	irr::scene::SMesh* mesh = new scene::SMesh();
+	scene::SMeshBuffer *buf = 0;
+	buf = new scene::SMeshBuffer();
+	mesh->addMeshBuffer(buf);
+	buf->drop();
+
+	buf->Vertices.reallocate(triangles.size() * 3);
+	buf->Vertices.set_used(triangles.size() * 3);
+
+	buf->Indices.reallocate(triangles.size() * 3);
+	buf->Indices.set_used(triangles.size() * 3);
+
 	for (unsigned int i = 0; i < triangles.size(); i++) {
 		core::triangle3df temp(triangles[i][0], triangles[i][1], triangles[i][2]);
 		core::vector3df norm = temp.getNormal();
 
-		irr::scene::SMesh* mesh = new scene::SMesh();
+		buf->Vertices[3 * i + 0] = video::S3DVertex(temp.pointA, norm, video::SColor(255, 0, 0, 0), core::vector2df(0.0f, 0.0f));
+		buf->Vertices[3 * i + 1] = video::S3DVertex(temp.pointB, norm, video::SColor(255, 0, 0, 0), core::vector2df(1.0f, 0.0f));
+		buf->Vertices[3 * i + 2] = video::S3DVertex(temp.pointC, norm, video::SColor(255, 0, 0, 0), core::vector2df(0.5f, 1.0f));
 
-		scene::SMeshBuffer *buf = 0;
-		buf = new scene::SMeshBuffer();
-		mesh->addMeshBuffer(buf);
-
-		buf->drop();
-
-		buf->Vertices.reallocate(3);
-		buf->Vertices.set_used(3);
-
-		buf->Vertices[0] = video::S3DVertex(temp.pointA, norm, video::SColor(255, 0, 0, 0), core::vector2df(0.0f, 0.0f));
-		buf->Vertices[1] = video::S3DVertex(temp.pointB, norm, video::SColor(255, 0, 0, 0), core::vector2df(1.0f, 0.0f));
-		buf->Vertices[2] = video::S3DVertex(temp.pointC, norm, video::SColor(255, 0, 0, 0), core::vector2df(0.5f, 1.0f));
-
-		buf->Indices.reallocate(3);
-		buf->Indices.set_used(3);
-
-		buf->Indices[0] = 0;
-		buf->Indices[1] = 1;
-		buf->Indices[2] = 2;
-
-		buf->recalculateBoundingBox();
-		irr::scene::IMeshSceneNode* node = m_smgr->addMeshSceneNode(mesh);
-		node->setMaterialFlag(video::EMF_BACK_FACE_CULLING, false);
-		node->setMaterialFlag(video::EMF_LIGHTING, false);
-		node->setAutomaticCulling(scene::EAC_OFF);
-		node->setName("Triangle" + m_nrOfNodes);
-		node->setID(m_nrOfNodes);
-		m_nrOfNodes++;
+		buf->Indices[3 * i + 0] = 3 * i + 0;
+		buf->Indices[3 * i + 1] = 3 * i + 1;
+		buf->Indices[3 * i + 2] = 3 * i + 2;
 	}
+	buf->recalculateBoundingBox();
+	irr::scene::IMeshSceneNode* node = m_smgr->addMeshSceneNode(mesh);
+	node->setMaterialFlag(video::EMF_BACK_FACE_CULLING, false);
+	node->setMaterialFlag(video::EMF_LIGHTING, false);
+	node->setAutomaticCulling(scene::EAC_OFF);
+	node->setName("Mesh" + m_nrOfNodes);
+	node->setID(m_nrOfNodes);
+	m_nrOfNodes++;
 }
 
 void Scene::addBox(core::vector3df position, float size, std::string name) {
 	scene::IMeshSceneNode* box = m_smgr->addCubeSceneNode(size, 0, m_nrOfNodes, position);
 	if (name.empty()) {
-		box->setName("Cube" + m_nrOfNodes);
+		box->setName("Box" + m_nrOfNodes);
 	}
 	else {
 		box->setName(name.c_str());
